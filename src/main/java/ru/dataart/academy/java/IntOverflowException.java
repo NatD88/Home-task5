44e542ab3274e2d52019a9df629842905ce0ee1c
Natalia Deviatkina <golovskaya@mail.ru>
2021-10-19 21:46:04 +0300

    added files for Home-task5

---
+++ src/main/java/ru/dataart/academy/java/IntOverflowException.java
@@ -0,0 +1,8 @@
+package ru.dataart.academy.java;
+
+public class IntOverflowException extends RuntimeException {
+
+    public IntOverflowException(String message) {
+        super(message);
+    }
+}
